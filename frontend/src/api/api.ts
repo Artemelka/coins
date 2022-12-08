81c30b19f8470f3b449c96f9f065d6e094ddb2bd
Artemelka <artemelka@wisebits.com>
2022-12-08 19:45:56 +0200

    remove mock

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -1,12 +1,7 @@
 import { ApiService } from '@/services/api';
-// import { MockApi } from './mock';
 
 const BASE_URL = '/api/mock'  //'https://manifity-coins-default-rtdb.firebaseio.com';
 
 export const Api = new ApiService({
   url: BASE_URL,
 });
-
-// export const Api = new MockApi({
-//   url: BASE_URL,
-// });
