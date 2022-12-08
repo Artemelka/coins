6eadc3c92f36e06415df7026df2b12dda73518f2
Artemelka <artemelka@wisebits.com>
2022-12-08 19:18:15 +0200

    add proxy

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -1,12 +1,12 @@
-// import { ApiService } from '@/services/api';
-import { MockApi } from './mock';
+import { ApiService } from '@/services/api';
+// import { MockApi } from './mock';
 
-const BASE_URL = 'https://manifity-coins-default-rtdb.firebaseio.com';
+const BASE_URL = '/api/mock'  //'https://manifity-coins-default-rtdb.firebaseio.com';
 
-// export const Api = new ApiService({
-//   url: BASE_URL,
-// });
-
-export const Api = new MockApi({
+export const Api = new ApiService({
   url: BASE_URL,
 });
+
+// export const Api = new MockApi({
+//   url: BASE_URL,
+// });
